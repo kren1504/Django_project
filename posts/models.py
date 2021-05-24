7c571a1b62dab33454b2b2f14c2d20425ee72daa
Karen Gonzalez <kren1504@hotmail.com>
2021-05-23 22:09:25 -0400

    Se creo un nuevo modelo de BD utilizando django.db import models

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Users(models.Model):
+
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=100)
+
+
+    frist_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+
+    bio = models.CharField(blank=True,max_length=100)
+
+    birthday = models.DateField(blank=True, null=True)
+
+    created = models.DateTimeField(auto_now_add=True) #cargar la fecha en la que se creo
+    modified = models.DateTimeField(auto_now=True) # guarda la fecha en que se edito por ultima vez
